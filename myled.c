b25c2416554fd4a369964294ff0c3bf2a4c1e643
Yusuke Nagano <s20c1085aa@s.chibakoudai.jp>
2021-12-28 07:29:23 +0000

    myled.c

--- myled.c
+++ myled.c
@@ -2,11 +2,13 @@
 
 static int _init init_mod(void)
 {
+       printk(KERN_INFO"%s is loaded.\n", __FILE__):
        return 0;
 }
 
 static void__exit cleanup_mod(void)
 {
+        printk(KERN_INFO"%s is unlodead.\n"__FILE__);
 }
 
 module_init(init_mod);
