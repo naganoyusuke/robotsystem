63a46c956693b941d64d1e82a76a161bf900dbe2
Yusuke Nagano <s20c1085aa@s.chibakoudai.jp>
2021-12-28 08:12:27 +0000

    myled.c

--- myled.c
+++ myled.c
@@ -1,14 +1,17 @@
-#includo<linux/module.h>
-
-static int _init init_mod(void)
+#include <linux/module.h>   
+MODULE_AUTHOR("Ryuichi Ueda and Yusuke Nagano");
+MODULE_DESCRIPTION("driver for LED control");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.0.1");
+static int __init init_mod(void)
 {
-       printk(KERN_INFO"%s is loaded.\n", __FILE__):
+       printk(KERN_INFO "%s is loaded.\n", __FILE__);
        return 0;
 }
 
-static void__exit cleanup_mod(void)
+static void __exit cleanup_mod(void)
 {
-        printk(KERN_INFO"%s is unlodead.\n"__FILE__);
+        printk(KERN_INFO "%s is unlodead.\n",__FILE__);
 }
 
 module_init(init_mod);
