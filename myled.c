ad0356d56c7c2704fda35a0687be0bd87ee598ba
Yusuke Nagano <s20c1085aa@s.chibakoudai.jp>
2022-01-01 07:47:16 +0000

    add a file

--- myled.c
+++ myled.c
@@ -1,6 +1,6 @@
 #include <linux/module.h>
 #include<linux/fs.h>
-
+#include<linux/cdev.h>
 
 MODULE_AUTHOR("Ryuichi Ueda and Yusuke Nagano");
 MODULE_DESCRIPTION("driver for LED control");
@@ -8,6 +8,18 @@ MODULE_LICENSE("GPL");
 MODULE_VERSION("0.0.1");
 
 static dev_t dev;
+static struct cdev cdv;
+
+static ssize_t led_write(struct file* filp, const char*buf, size_t count, loff_t* pos)
+{
+	printk(KERN_INFO "led_write is called\n");
+	return 1;
+}
+
+static struct file_operations led_fops = {
+	.owner = THIS_MODULE,
+	.write = led_write
+};
 
 
 static int __init init_mod(void)
@@ -19,11 +31,19 @@ static int __init init_mod(void)
 		return retval;
 	}
 	printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
+
+	cdev_init(&cdv, &led_fops);
+	retval = cdev_add(&cdv, dev, 1);
+	if(retval < 0){
+		printk(KERN_ERR "cdev_add failed. major:%d, minor:%d",MAJOR(dev),MINOR(dev));
+		return retval;
+	}
        return 0;
 }
 
 static void __exit cleanup_mod(void)
 {
+	cdev_del(&cdv);
 	unregister_chrdev_region(dev, 1);
         printk(KERN_INFO "%s is unlodead. major:%d\n",__FILE__,MAJOR(dev));
 }
