03d692fd28fada848b4c29ffe972a213bc6bd5eb
Yusuke Nagano <s20c1085aa@s.chibakoudai.jp>
2022-01-02 15:44:37 +0000

    add a faile

--- myled.c
+++ myled.c
@@ -20,7 +20,14 @@ static ssize_t led_write(struct file* filp, const char*buf, size_t count, loff_t
 	char c;
 	if(copy_from_user(&c,buf,sizeof(char)))
 	return -EFAULT;
-	printk(KERN_INFO "receive %c\n", c);
+//	printk(KERN_INFO "receive %c\n", c);
+	if(c =='0'){
+		gpio_base[10] = 1 << 25;
+	}else if (c == '1'){
+		gpio_base[7] =1 << 25;
+	}
+
+
 	return 1;
 }
 static ssize_t sushi_read(struct file* file, char* buf, size_t count, loff_t* pos)
